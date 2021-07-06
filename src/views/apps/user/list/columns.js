eca7606977fcab10099952c76ca125a911f94feb
JE1999 <je99alvarez@gmail.com>
2021-07-06 10:11:09 -0400

    Initial commit

---
+++ src/views/apps/user/list/columns.js
@@ -0,0 +1,154 @@
+// ** React Imports
+import { Link } from 'react-router-dom'
+
+// ** Custom Components
+import Avatar from '@components/avatar'
+
+// ** Store & Actions
+import { getUser, deleteUser } from '../store/action'
+import { store } from '@store/storeConfig/store'
+
+// ** Third Party Components
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
+import { Slack, User, Settings, Database, Edit2, MoreVertical, FileText, Trash2, Archive } from 'react-feather'
+
+// ** Renders Client Columns
+const renderClient = row => {
+  const stateNum = Math.floor(Math.random() * 6),
+    states = ['light-success', 'light-danger', 'light-warning', 'light-info', 'light-primary', 'light-secondary'],
+    color = states[stateNum]
+
+  if (row.avatar.length) {
+    return <Avatar className='mr-1' img={row.avatar} width='32' height='32' />
+  } else {
+    return <Avatar color={color || 'primary'} className='mr-1' content={row.fullName || 'John Doe'} initials />
+  }
+}
+
+// ** Renders Role Columns
+const renderRole = row => {
+  const roleObj = {
+    subscriber: {
+      class: 'text-primary',
+      icon: User
+    },
+    maintainer: {
+      class: 'text-success',
+      icon: Database
+    },
+    editor: {
+      class: 'text-info',
+      icon: Edit2
+    },
+    author: {
+      class: 'text-warning',
+      icon: Settings
+    },
+    admin: {
+      class: 'text-danger',
+      icon: Slack
+    }
+  }
+
+  const Icon = roleObj[row.role] ? roleObj[row.role].icon : Edit2
+
+  return (
+    <span className='text-truncate text-capitalize align-middle'>
+      <Icon size={18} className={`${roleObj[row.role] ? roleObj[row.role].class : ''} mr-50`} />
+      {row.role}
+    </span>
+  )
+}
+
+const statusObj = {
+  pending: 'light-warning',
+  active: 'light-success',
+  inactive: 'light-secondary'
+}
+
+export const columns = [
+  {
+    name: 'Nombre',
+    minWidth: '320px',
+    selector: 'fullName',
+    sortable: true,
+    cell: row => (
+      <div className='d-flex justify-content-left align-items-center'>
+        {renderClient(row)}
+        <div className='d-flex flex-column'>
+          <Link
+            to={`/apps/user/view/${row.id}`}
+            className='user-name text-truncate mb-0'
+            onClick={() => store.dispatch(getUser(row.id))}
+          >
+            <span className='font-weight-bold'>{row.fullName}</span>
+          </Link>
+          <small className='text-truncate text-muted mb-0' style={{marginTop: '4px'}}>001-0001110-1</small>
+        </div>
+      </div>
+    )
+  },
+  {
+    name: 'Teléfono',
+    minWidth: '160px',
+    selector: 'email',
+    sortable: true,
+    cell: row => '809-220-1111'
+  },
+  {
+    name: 'Provincia',
+    minWidth: '235px',
+    selector: 'currentPlan',
+    sortable: true,
+    cell: row => 'Santo Domingo'
+  },
+  {
+    name: 'Municipio',
+    minWidth: '235px',
+    selector: 'status',
+    sortable: true,
+    cell: row => 'Los Alcarrizos'
+  },
+  {
+    name: 'Rol',
+    minWidth: '172px',
+    selector: 'role',
+    sortable: true,
+    cell: row => renderRole(row)
+  },
+  {
+    name: 'Acciones',
+    minWidth: '50px',
+    cell: row => (
+      <UncontrolledDropdown>
+        <DropdownToggle tag='div' className='btn btn-sm'>
+          <MoreVertical size={14} className='cursor-pointer' />
+        </DropdownToggle>
+        <DropdownMenu right>
+          <DropdownItem
+            tag={Link}
+            to={`/apps/user/view/${row.id}`}
+            className='w-100'
+            onClick={() => store.dispatch(getUser(row.id))}
+          >
+            <FileText size={14} className='mr-50' />
+            <span className='align-middle'>Details</span>
+          </DropdownItem>
+          <DropdownItem
+            tag={Link}
+            to={`/apps/user/edit/${row.id}`}
+            className='w-100'
+            onClick={() => store.dispatch(getUser(row.id))}
+          >
+            <Archive size={14} className='mr-50' />
+            <span className='align-middle'>Edit</span>
+          </DropdownItem>
+          <DropdownItem className='w-100' onClick={() => store.dispatch(deleteUser(row.id))}>
+            <Trash2 size={14} className='mr-50' />
+            <span className='align-middle'>Delete</span>
+          </DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    )
+  }
+]
