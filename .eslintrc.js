f05a592770b17d19646d76c92009391b8804ac90
Gustavo Valverde <gustavo@iterativo.do>
2021-08-26 00:48:39 -0400

    fix: do not lint with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: "@imaginary-cloud/react",
+  parser: "babel-eslint",
 
   env: {
     node: true,
@@ -7,9 +8,8 @@ module.exports = {
     browser: true
   },
 
-  parser: "babel-eslint",
   parserOptions: {
-    ecmaVersion: 6,
+    ecmaVersion: 2018,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
